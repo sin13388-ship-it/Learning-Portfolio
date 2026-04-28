db3bbf75cee449cd341099f18e06a6d2653ab8d2
Vera <sin13388@iot.com>
2026-04-28 15:21:41 +0800

    pointer exercise

--- 01_C-Code/ArrayFunction.c
+++ 01_C-Code/ArrayFunction.c
@@ -10,6 +10,12 @@ void evaluate_sum_with_pointer();
 void string_pointers();
 void find_maximum();
 int *maximum(int *, int);
+void using_pointer_to_change_array_elements();
+void using_index_to_find_max_min();
+void count_lower_case_characters();
+void matrix_sum();
+void pointer_of_pointer();
+
 
 int main(){	
 	//array_update();	
@@ -18,7 +24,13 @@ int main(){
 	//array_and_pointers_and_retrieve2();
 	//evaluate_sum_with_pointer();
 	//string_pointers();
-	find_maximum();
+	//find_maximum();
+	//using_pointer_to_change_array_elements();
+	//using_index_to_find_max_min();
+	//count_lower_case_characters();
+	//matrix_sum();
+	
+	pointer_of_pointer();
 }
 
 void array_update(){	
@@ -129,7 +141,6 @@ void string_pointers(){
 	puts(ptr);
 	
 }
-
 void find_maximum(){
 	
 	int array[6]={23,37,58,97,75,92};
@@ -145,21 +156,114 @@ void find_maximum(){
 	printf("The maximum is %d\n", *ptr);	
 	
 }
-
-
-
-int *maximum(int *arr, int length){
-	
+int *maximum(int *arr, int length){	
 	int *max ;
-	max=arr;
-	
+	max=arr;	
 	int i=0;
 	for(i=1; i<length; i++){
 		if (*max < *(arr+i ) )*max=*(arr+i);
 	}
-	return max;
+	return max;	
+}
+void using_pointer_to_change_array_elements(){
+	
+	int IOT[5]={31,17,33,22,16};
+	int *ptr=IOT;
+	int i;
+	printf("IOT[5]={");
+	for(i=0; i<5; i++){
+		if(i!=4){
+			printf("%d, ",*(ptr+i));
+		}
+		else{
+			printf("%d}\n ",*(ptr+i));
+		}		
+	}
+	printf("\nAfter adding 7 to the value\n");
+	printf("IOT[5]={");
+	for(i=0; i<5; i++){
+		if(i!=4){
+			printf("%d, ",*(ptr+i)+7);
+		}
+		else{
+			printf("%d}\n ",*(ptr+i)+7);
+		}		
+	}
+}
+void using_index_to_find_max_min(){
+	int A[6]={11,48,30,17,62,37};
+	int i, max, min;
+	i=0;
+	max=min=0;	
+	//此處的max, min 是用來儲存index 	
+	for(i=0;i<6;i++){
+		if(*(A+i) > *(A+max)) max=i;
+		if(*(A+i) < *(A+min)) min=i;
+	}
+	printf("The max = %d\n", *(A+max));
+	printf("The min = %d\n", *(A+min));		
+}
+void count_lower_case_characters(){	
+	/*for 的判斷可以用!=*/
+	char *ptr="We are best friends.";
+	int i, count=0;
+	/*找到字串結束*/
+	for(i=0; *(ptr+i)!='\0'; i++){		
+		if(*(ptr+i)>=97 && *(ptr+i)<=122) count++;	
+	}	
+	printf("Number of lowercase in string is %d\n", count);	
+}
+void matrix_sum(){
+	
+	int i,j;
+	int A[2][3]={{3,3,4},{6,2,7}};
+	int B[2][3]={{2,-1,3},{3,1,2}};
+	int C[2][3];
+	int D[2][3];	
+	int (*ptr)[3]=D; //不管如何都要指向一個陣列，不管在這還是之後 
+	
+	/*矩陣相加*/
+	
+	int *ptrA, *ptrB, *ptrC;
+	for(i=0; i<2; i++){
+		ptrA=A[i];
+		ptrB=B[i];
+		ptrC=C[i];
+		for(j=0;j<3;j++)
+		{
+			*(ptrC+j)=*(ptrA+j)+*(ptrB+j);
+			*(*(ptr+i)+j)=*(ptrA+j)-*(ptrB+j);
+		}		
+	}	
+	printf("A+B=\n");
+	
+	for(i=0; i<2; i++){
+		ptrC=C[i];
+		for(j=0;j<3;j++)
+		{
+			printf("%3d",*(ptrC+j));
+		}		
+		printf("\n");
+	}	
+	printf("A-B=\n");
+	
+	for(i=0; i<2; i++){
+		for(j=0;j<3;j++)
+		{
+			printf("%3d",*(*(ptr+i)+j));
+		}		
+		printf("\n");
+	}	
+}
+void pointer_of_pointer(){
+	
+	int p=20;
+	int *ptr1, **ptr2;
+
+	ptr1=&p; /*指向指標*/ 
+	ptr2=&ptr1; /*指向指標的指標*/
+	printf("p=%d, &p=%p, *ptr1=%d, ptr1=%p, &ptr1=%p\n", p, &p, *ptr1, ptr1, &ptr1);
+	printf("**ptr2=%d, *ptr2=%p, ptr2=%p, &ptr2=%p",**ptr2, *ptr2, ptr2, &ptr2); //20, ptr1, 
 	
 }
-
-
 
