6d987368a72eaf60fb0a80548373df02dcd2eb95
Vera <sin13388@gmail.com>
2026-05-16 22:34:40 +0800

    Java IO Exception 實作題(AI 建議修改後)

--- 03_Java-Kotlin/Java/Code/MarchingOntoThePathOfJava/Ch20/IO_Exception/src/main/java/com/vera/ioexception/Handsontest_05.java
+++ 03_Java-Kotlin/Java/Code/MarchingOntoThePathOfJava/Ch20/IO_Exception/src/main/java/com/vera/ioexception/Handsontest_05.java
@@ -1,22 +1,20 @@
 package com.vera.ioexception;
 
-public class handsontest_05 {
-    static void main(String[] args) {
+public class Handsontest_05 {
+    public static void main(String[] args) {
         int[] ages = {12,19,67};
+        Vote parseAge=new Vote();
         for(int i :ages)
         {
             try
             {
-                Vote parseAge=new Vote();
-
                 parseAge.parse(i);
                 System.out.println(i+"歲歡迎投票");
-
             }
             catch (UnderageException e)
             {
                 System.out.println(e.getAge()+"歲的年齡太輕");
-                System.out.println("Error! "+e+": "+e.getMsg());
+                System.out.println("Error! "+e.getClass().getSimpleName()+": "+e.getMessage());
             }
         }
         System.out.println("測試年齡愉快");
@@ -28,14 +26,11 @@ class Vote {
     }
 }
 class UnderageException extends Exception{
-    private String msg;
+
     private int age;
     UnderageException(String msg, int age){
-        this.msg=msg;
+        super(msg); //直接使用exception 本來的
         this.age=age;
-    }
-    String getMsg(){
-        return msg;
     }
     int getAge()
     {
