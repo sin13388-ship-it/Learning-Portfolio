06ae5db4fb5db413146b54aee190124c85873540
Vera <sin13388@iot.com>
2026-05-26 12:33:46 +0800

    ColorLed_Exercise

---
+++ 04_DigitalLogic/arduino_project/DL_0526_Exercise/ColortLed/ColortLed.ino
@@ -0,0 +1,33 @@
+void ledUpdates(int state);
+void ledColorUp();
+
+#define startPin 10
+const int pins[3]={10,11,12};
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int i=0;i<3;i++){
+    pinMode(startPin+i, OUTPUT); //using pin9~pin11 as the led's output
+    digitalWrite(startPin+i, LOW);
+  }  
+}
+
+void ledColorUp(){
+  byte ledState=0;
+    for(ledState=0;ledState<8;ledState++){
+      ledUpdates(ledState);
+      delay(1000); }
+}
+
+void ledUpdates(int state){
+  byte pinState=(byte) (state & 0x07);
+  digitalWrite(10, ((pinState>>0) & 0x01));
+  digitalWrite(11, ((pinState>>1) & 0x01));
+  digitalWrite(12, ((pinState>>2) & 0x01));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ledColorUp();
+
+}
