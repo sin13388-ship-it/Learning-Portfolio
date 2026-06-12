f6f4bc9bfae66da7a7af03347c3c9b1e3afffad1
Vera <sin13388@iot.com>
2026-06-12 14:57:41 +0800

    Kotlin homework, web design

--- 08_Kotlin/kotlin_project/KT_3/src/Ex5.kt
+++ 08_Kotlin/kotlin_project/KT_3/src/Ex5.kt
@@ -1,40 +1,29 @@
 fun main() {
 
-    var q1 = studentMap("Mary", listOf(92, 83, 67)).run(::println)
-    q1 = studentMap("John", listOf(81, 68, 70)).run(::println)
-    q1 = studentMap("Nancy", listOf(93, 89, 73)).run(::println)
+    //想印出 Map 同時保留 Map 變數，要用 .also 而不是 .run
+    println("Q1====")
+    studentMap("Mary", listOf(92, 83, 67))
+    studentMap("John", listOf(81, 68, 70))
+    studentMap("Nancy", listOf(93, 89, 73))
 
-
+    println("Q2====")
     val lst = listOf(1, 2, 3, 4, 5, 6)
-
     val f:(List<Int>)->List<Int> ={
-        l ->
-        l.filter { it %2 ==0 }
-
+        l -> l.filter { it %2 ==0 }
     }
     val s : (List<Int>)->List<Int> ={
-        l ->
-           var result: MutableList<Int> = mutableListOf()
-            l.forEach {
-                result.add(it *it)
-            }
-        result
+        l ->l.map {(it *it)}
     }
-
-    //val q2=Math(lst, f(lst), s(lst)).run(::println)
-
+    math(lst, f, s).also(::println)
 }
 fun studentMap(name: String, scores: List<Int>): MutableMap<String, Any>{
-
-    var stdMap =mutableMapOf<String, Any>(Pair("name",name),
-        Pair("sum",scores.sum()),
-        Pair("average",scores.average()))
+    val stdMap =mutableMapOf<String, Any>("name" to name,
+        "sum" to scores.sum(),
+        "average" to scores.average()).also {
+            println("name=${name}, sum =${it["sum"]}, average= ${it["average"]}") }
     return stdMap
 }
-fun Math(data: List<Int>, filter:(List<Int>) -> List<Int>, square:(List<Int>) -> List<Int>): List<Int>{
-
-    var lst =filter(data)
-
+fun math(data: List<Int>, filter:(List<Int>) -> List<Int>, square:(List<Int>) -> List<Int>): List<Int>{
+    val lst =filter(data)
     return square(lst)
-
 }
