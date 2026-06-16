07d7e6942d944a6d405081faeb1956897ef6158f
Vera <sin13388@iot.com>
2026-06-16 10:23:55 +0800

    kotlinb class

--- 08_Kotlin/kotlin_project/KT_3/src/Class_1.kt
+++ 08_Kotlin/kotlin_project/KT_3/src/Class_1.kt
@@ -25,6 +25,49 @@ fun main() {
     data1.str=""
     println(data1.str)
 
+    /*getter, setter exercise*/
+    var myPlayer=Player()
+
+    myPlayer.apply {
+        println("myplayer: name= $name, id= $id, email= $email ")
+        email="8" //會出現error
+    }
+    myPlayer.apply {
+        name="John"
+        id=20 //會出現error
+        email="ABC@gmail.com"
+        changePhone("99008866")
+        attrPrint()
+    }
+
+    println("initialize===")
+
+    /*class initialize(constructure) */
+    var newPayer2=NewPlayer1("Lily",20,false)
+    newPayer2.attPrint()
+
+    var newPayer3=NewPlayer1("Lilyyyyyyyyyyyyyy",20,false)
+    newPayer3.attPrint() //初始化給的變數不會納入getter, setter 的判斷
+
+    //此時的更改在getter, setter 裡
+    newPayer3.apply {
+        name="Lilyyyyyyyyyyyyyy"
+    }
+
+    println()
+    var newPlayer4=NewPlayer2("John", 33, 90)
+    newPlayer4.attPrint()
+    newPlayer4=NewPlayer2("Mary",45) //會建立一個全新的物件
+    newPlayer4.attPrint()
+
+    println()
+    var newPlayer5=NewPlayer3("John", 43, 88)
+    newPlayer5.attPrint()
+
+    newPlayer5=NewPlayer3("Jane")
+    newPlayer5.attPrint()
+
+
 }
 
 
--- 08_Kotlin/kotlin_project/KT_3/src/PlayerClass.kt
+++ 08_Kotlin/kotlin_project/KT_3/src/PlayerClass.kt
@@ -36,3 +36,85 @@ class ToUpper{
             return "transfer data : $field"
         }
 }
+
+class Player{
+    /*姓名轉大寫*/
+    /*Mail 轉小寫*/
+    private  var phone="123456"
+    /*getter , setter 要緊接於下方*/
+    var name="Mary"
+        get()= field.uppercase()
+        set(value){field=value.trim()}
+    var id:Int =1
+        get()=field+100
+        set(value){
+            if(value>10){
+                println("id is error")
+                field=0
+            }else{
+                field=value
+            }
+        }
+    var email="AAA@gmail.com"
+        get()=field.lowercase()
+        /*可以新增一些資料驗證的方法*/
+        set(value){
+            if(value.length<5){
+                println("Length is error")
+                field="error"
+            }else{
+                field=value
+            }
+        }
+    fun attrPrint(){
+        println("name= $name, id= $id, email= $email, phone=$phone ")
+    }
+    fun changePhone(phone: String){
+        this.phone=phone
+    }
+}
+
+class NewPlayer1(_name:String, _age: Int, _isBlessed: Boolean){
+    /*attribute 另外拉出來宣告
+    * 如果要用getter, setter 的話，還是需要用此方法
+    * */
+    var name=_name
+        /*初始化的數值不會進入到getter/ setter 的判斷*/
+        get()= field.lowercase()
+        set(value){
+            if(value.length >10){
+                println("name is too long")
+                field="Error"
+            }else{
+                field=value
+            }
+        }
+    var age=_age
+    var isBlessed=_isBlessed
+    init{
+        println("do init")
+    }
+    fun attPrint(){
+        println("name= $name, age= $age, isBlessed= $isBlessed")
+    }
+}
+
+class NewPlayer2(var name: String, var age: Int, var healthPoint: Int=80)
+{
+    //Health point 有給預設值
+    //主建構子已經宣告，此處就不用額外宣告
+    fun attPrint(){
+        println("name= $name, age= $age, healthPoint= $healthPoint")
+    }
+}
+
+class NewPlayer3(_name: String, var age: Int=50, var healthPoint: Int=70)
+{
+    //某個參數想使用屬性的話，可以不用在主建構式用var/ val
+    var name=_name
+        get()=field.uppercase()
+    fun attPrint(){
+        println("name= $name, age= $age, healthPoint= $healthPoint")
+    }
+
+}
