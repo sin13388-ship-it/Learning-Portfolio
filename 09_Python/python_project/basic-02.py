15ae16558bc31368072f0f9d9bad8069f3021d81
Vera <sin13388@iot.com>
2026-06-23 15:41:31 +0800

    Pyhton exercise basic

---
+++ 09_Python/python_project/basic-02.py
@@ -0,0 +1,17 @@
+# Python 基本
+
+import math
+
+counter =100
+miles =1000.0
+name = "John"
+flag1=True
+flag2=False
+
+print(f"counter = {counter}")
+print("miles ", str(miles))
+print("miles ", str(miles), sep="&", end=" ") #sep 可以更換字元, end 可以更換結尾字元; 預設是\n
+print("test1")
+print("name ", name, sep=",", end="\n") #sep 可以更換字元, end 可以更換結尾字元; 預設是\n
+print("test2")
+
